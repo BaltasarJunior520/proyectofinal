720a0aa35c36a22fc2e4aad9e999bd52e5962a3b
BaltasarJunior520 <meyliyui123@gmail.com>
2026-05-22 08:54:27 -0400

    fix: cambiar shebang a #!/bin/sh y hacer ejecutable init.sh para compatibilidad con Alpine

--- basededatos/init.sh
+++ basededatos/init.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 # Obtener directorio del script o usar /basededatos si es Docker
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" && pwd)"
+DIR="$(cd "$(dirname "${BASH_SOURCE:-$0}")" && pwd)"
 if [ -d "/basededatos" ]; then
   SQL_DIR="/basededatos"
 else
